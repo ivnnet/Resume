08506d7bdafbe52fe0e4c7e2cec5acd8dd7bf228
dropa <altfortts@gmail.com>
2025-03-13 17:20:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ function login() {
     const password = document.getElementById("password").value;
     
     if (username === "admin" && password === "password") {
+        localStorage.setItem("loggedIn", "true");
         window.location.href = "resume.html";
     } else {
         document.getElementById("error-msg").textContent = "Invalid credentials!";
     }
+}
+
+// Logout function
+function logout() {
+    localStorage.removeItem("loggedIn");
+    window.location.href = "index.html";
 }
